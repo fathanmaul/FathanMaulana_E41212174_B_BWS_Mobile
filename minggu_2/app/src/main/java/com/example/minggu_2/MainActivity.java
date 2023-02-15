0e1ff00e4bd6759f84b96c94253b6029a564ff04
FlyingCat17 <fthnmrh012@gmail.com>
2023-02-15 16:36:48 +0700

    add minggu 2

---
+++ minggu_2/app/src/main/java/com/example/minggu_2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.minggu_2;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar act = getSupportActionBar();
+        act.hide();
+        setContentView(R.layout.activity_main);
+    }
+}
