719b0751953af0990064f807d173c58e266a00a4
FlyingCat17 <fthnmrh012@gmail.com>
2023-02-25 19:17:39 +0700

    minggu 3

--- minggu_2/app/src/main/java/com/example/minggu_2/MainActivity.java
+++ minggu_2/app/src/main/java/com/example/minggu_2/MainActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.zip.Inflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
         ActionBar act = getSupportActionBar();
         act.hide();
         setContentView(R.layout.activity_main);
+
+//        Button btn = (Button) findViewById(R.id.button_send);
+//        EditText txt_1 = (EditText) findViewById(R.id.txt);
+//
+//        btn.setOnClickListener(v -> {
+//            String str = txt_1.getText().toString();
+//            Intent intent = new Intent(MainActivity.this, Activity_2.class);
+//            intent.putExtra("message_key", str);
+//            startActivity(intent);
+//            txt_1.setText("");
+//        });
     }
 }
---
+++ minggu_3/app/src/main/java/com/example/minggu_3/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.minggu_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.minggu_3.model.Mahasiswa;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void addData(){
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Fathan Maulana", "E41212174", "085604982258"));
+        mahasiswaArrayList.add(new Mahasiswa("Lutfi Zahroni", "E41212124", "081981727626"));
+        mahasiswaArrayList.add(new Mahasiswa("Muhammad Sulaiman", "E41212122", "082371823718"));
+    }
+}
